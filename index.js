d20f9d28ea4f860420b87384570bccfbabb5fc7c
LinckA05 <143820870+LinckA05@users.noreply.github.com>
2023-09-08 14:09:41 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const length = parseInt(prompt('Enter the length: '));
+const width = parseInt(prompt('Enter the width: '));
+
+const area = length * width;
+const parameter = (length*2)+(width*2);
+
+console.log(`The area is ${area}`);
+console.log(`The parameter is ${parameter}`);
